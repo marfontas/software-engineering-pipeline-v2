b53711c6997d9544d9977b99a7eb86791a23d297
marfontas <mar.fontas01@estudiant.upf.edu>
2026-05-11 16:30:31 +0200

    test

---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+from calculator import add, sub, multiply
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_sub():
+    assert sub(10, 4) == 6
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+    assert multiply(0, 5) == 0
